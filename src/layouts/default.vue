a08e7b733d425363abf24060b3c206db65ae2bad
ttt1091 <tez1091@yahoo.co.jp>
2021-05-27 18:14:07 +0900

    create

---
+++ src/layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <nav
+      class="navbar header has-shadow is-success"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          href="/"
+        >
+          <img
+            src="~assets/buefy.png"
+            alt="Buefy"
+            height="28"
+          >
+        </a>
+
+        <div class="navbar-burger">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+    </nav>
+
+    <section class="main-content columns">
+      <Mainmenu />
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Mainmenu from '~/components/Mainmenu'
+export default {
+  components: {
+    Mainmenu
+  },
+}
+</script>
