c744239ef887317c9be42d7cf95886de73cde43e
ttt1091 <tez1091@yahoo.co.jp>
2021-06-08 19:13:20 +0900

    change

--- src/pages/articles/_slug.vue
+++ src/pages/articles/_slug.vue
@@ -23,6 +23,11 @@ export default {
   head() {
     return {
       title: this.article.title,
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: this.article.description }
+      ]
     }
   },
   fetch ({store}) {
