25bcf58cd2874456e87f5984d6a7e22efeb1097c
ttt1091 <tez1091@yahoo.co.jp>
2021-06-15 19:54:53 +0900

    change

--- src/pages/girls/_slug.vue
+++ src/pages/girls/_slug.vue
@@ -54,19 +54,19 @@
     },
     async asyncData({ $axios, params }) {
       const url = '/girl/?streamid='+params.slug+'&profile=1'
-      const response = await $axios.$get(url)
+      const profile = await $axios.$get(url)
       const channelUrl = '/girl/?streamid='+params.slug+'&channel=1'
       const channelResponse = await $axios.$get(channelUrl)
       return {
-        name: response.data.profile_data.name,
-        age: response.data.profile_data.age,
-        info: response.data.profile_data.info,
+        name: profile.data.profile_data.name,
+        age: profile.data.profile_data.age,
+        info: profile.data.profile_data.info,
         channel_info: channelResponse.data.channel_data.info,
-        icon: response.data.profile_data.icon,
-        image: response.data.profile_data.image,
+        icon: profile.data.profile_data.icon,
+        image: profile.data.profile_data.image,
         channel_image: channelResponse.data.channel_data.image,
         channelid: channelResponse.data.channel_data.channelid,
-        fc2id: response.data.profile_datafc2id,
+        fc2id: profile.data.profile_datafc2id,
         start: channelResponse.data.channel_data.start
       }
     },
